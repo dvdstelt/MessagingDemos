563d98cc94015c1eb86746c3fd2c536bcc218032
Dennis <dvdstelt@gmail.com>
2019-06-18 10:22:19 +0200

    Initial commit of demos

---
+++ src/02 NServiceBusDemo/Receiver/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using NServiceBus;
+using Shared;
+
+namespace Receiver
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var endpointConfiguration = Configuration.GetConfiguration("Receiver");
+            endpointConfiguration.Recoverability().Immediate(s => s.NumberOfRetries(1));
+            endpointConfiguration.Recoverability().Delayed(s =>
+            {
+                s.NumberOfRetries(3);
+                s.TimeIncrease(TimeSpan.FromSeconds(2));
+            });
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration);
+
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/03 Publish Subscribe Demo/Billing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace Billing
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.Title = "Billing";
+
+            var endpointConfiguration = new EndpointConfiguration("Billing");
+
+            endpointConfiguration.UseTransport<LearningTransport>();
+            endpointConfiguration.SendFailedMessagesTo("error");
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
